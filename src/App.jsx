ef7e3b108e241ec25ac003ef8d4ca0bb0f4398db
W. James Dittmar <wjdittmar@gmail.com>
2024-04-07 17:46:50 -0700

    fix spacing and lint errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, useMatch } from 'react-router-dom'
+import { Routes, Route, useMatch } from 'react-router-dom'
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
@@ -7,39 +7,39 @@ import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
 const mapResults = (({ results }) => results.map(({ url, name }) => ({
-  url,
-  name,
-  id: parseInt(url.match(/\/(\d+)\//)[1])
+	url,
+	name,
+	id: parseInt(url.match(/\/(\d+)\//)[1])
 })))
 
 const App = () => {
-  const match = useMatch('/pokemon/:name')
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
+	const match = useMatch('/pokemon/:name')
+	const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
 
-  if (isLoading) {
-    return <LoadingSpinner />
-  }
-  if (error) {
-    return <ErrorMessage error={error} />
-  }
+	if (isLoading) {
+		return <LoadingSpinner />
+	}
+	if (error) {
+		return <ErrorMessage error={error} />
+	}
 
-  let next = null
-  let previous = null
+	let next = null
+	let previous = null
 
-  if (match && match.params) {
-    const pokemonId = pokemonList.find(({ name }) => name === match.params.name).id
-    previous = pokemonList.find(({ id }) => id === pokemonId - 1)
-    next = pokemonList.find(({ id }) => id === pokemonId + 1)
-  }
+	if (match && match.params) {
+		const pokemonId = pokemonList.find(({ name }) => name === match.params.name).id
+		previous = pokemonList.find(({ id }) => id === pokemonId - 1)
+		next = pokemonList.find(({ id }) => id === pokemonId + 1)
+	}
 
-  return (
-    <Routes>
-      <Route exact path="/" element={<PokemonList pokemonList={pokemonList} />} />
-      <Route exact path="/pokemon/:name" element={
-        <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
-      } />
-    </Routes>
-  )
+	return (
+		<Routes>
+			<Route exact path="/" element={<PokemonList pokemonList={pokemonList} />} />
+			<Route exact path="/pokemon/:name" element={
+				<PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
+			} />
+		</Routes>
+	)
 }
 
 export default App
